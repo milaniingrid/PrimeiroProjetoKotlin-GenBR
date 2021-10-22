bc32a5cb81d2399d4254739c614ee1d332627432
Milaniingrid <milanisingrid@gmail.com>
2021-10-22 15:22:18 -0300

    treino kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("<???> Hi")
+    println("<Madalena> Hi, how are you?")
+    println("<???> not bad ")
+    println("<Madalena> What is your name?")
+    println("<???> My name is Milani")
+    println("<Madalena> Nice to meet you, Milani")
+    println("<Milani> Nice to meet you too")
+
+    // Try adding program arguments at Run/Debug configuration
+    println("Program arguments: ${args.joinToString()}")
+}
